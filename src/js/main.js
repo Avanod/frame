de62a9dcdbdce641df573c0ca85cbb4c50c326f2
Nima Kaviyani <nima.kaviyani@gmail.com>
2022-11-06 13:51:17 +0330

    fix: width

--- src/js/main.js
+++ src/js/main.js
@@ -110,12 +110,10 @@ startButton.addEventListener('click', () => {
 stopButton.addEventListener('click', () => {
   stopRecording([videoStreamState, audioStreamState]);
 }, false);
-3;
 
 // Haydeh
 const mouseEvents = () => {
   let moved, clicked, startPosition, element;
-
   let mousedownListener = (event) => {
     const x = event.pageX;
     const y = event.pageY;
@@ -126,24 +124,30 @@ const mouseEvents = () => {
   let mousemoveListener = (event) => {
     const x = event.pageX;
     const y = event.pageY;
+    const innerWidth = window.innerWidth;
+    const innerHeight = window.innerHeight;
     if (clicked) {
       event.preventDefault();
       if (!moved) element = createElement();
       if (!element) return;
+      console.log({x, innerWidth});
+      console.log({y, innerHeight});
       // Style
       //// Background color of element
       element.style.backgroundColor = 'black';
       //// CSS position of element
       element.style.position = 'absolute';
       //// Position of element
-      element.style.top = `${startPosition.y < y ? startPosition.y : y}px`;
-      element.style.right = `${startPosition.x < x ? startPosition.x : x}px`;
-      element.style.bottom = `${y < startPosition.y ? y : startPosition.y}px`;
-      element.style.left = `${x < startPosition.x ? x : startPosition.x}px`;
+      element.style.top = `${startPosition.y < y ? startPosition.y : (y < 5 ? 5 : y)}px`;
+      element.style.right = `${startPosition.x < x ? startPosition.x : (x > innerWidth ? innerWidth - 5 : x)}px`;
+      element.style.bottom = `${y < startPosition.y ? startPosition.y : (y > innerHeight ? innerHeight - 5 : y)}px`;
+      element.style.left = `${x < 5 ? 5 : (x < startPosition.x ? x : startPosition.x)}px`;
       //// Width and height of element
-      element.style.width = `${startPosition.x > x ? startPosition.x - x : x - startPosition.x}px`;
-      element.style.height = `${startPosition.y > y ? startPosition.y - y : y - startPosition.y}px`;
+      element.style.width = `${startPosition.x > x ? (x < 0 ? startPosition.x - 5 : startPosition.x - x) : (x > innerWidth ? innerWidth - startPosition.x - 5 : x - startPosition.x)}px`;
+      element.style.height = `${startPosition.y > y ? (y < 0 ? startPosition.y - 5 : startPosition.y - y) : (y > innerHeight ? innerHeight - startPosition.y - 5 :  y - startPosition.y)}px`;
       ////////////////////////////////////////////
+      console.log(startPosition.x > x ? (x < 0 ? startPosition.x - 5 : startPosition.x - x) : (x > innerWidth ? innerWidth - startPosition.x - 5 : x - startPosition.x))
+      console.log(startPosition.y > y ? (y < 0 ? startPosition.y - 5 : startPosition.y - y) : (y > innerHeight ? innerHeight - startPosition.y - 5 :  y - startPosition.y))
       moved = true;
     }
   };
@@ -167,11 +171,23 @@ const mouseEvents = () => {
 
 mouseEvents();
 
+// Create element
 const createElement = () => {
+  // Create elements
   const element = document.createElement('div');
+  const close = document.createElement('div');
+  close.innerText = '✖';
+  // Set attributes
+  close.setAttribute('class', 'close');
   element.setAttribute('id', `mask-element-${Math.random().toString(16).slice(2)}`);
-  element.addEventListener('click', () => removeElement(element));
+  // Add event listener to close
+  close.addEventListener('click', () => removeElement(element));
+  // Append close to element
+  element.appendChild(close);
+  // Append element to body
   document.body.appendChild(element);
   return element;
 };
+
+// Remove element
 const removeElement = (element) => element.remove();
