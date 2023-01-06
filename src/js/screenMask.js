5dd6797e0bf9bbf458e1e7d1f1706b5ea075c1d5
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-01-06 20:55:32 +0330

    fix: cleanup and add release memory

--- src/js/screenMask.js
+++ src/js/screenMask.js
@@ -1,70 +1,8 @@
-const direction = getComputedStyle(document.querySelector('body')).direction;
-
 class ScreenMask {
-  init = (initial) => {
-    console.log({direction});
-    if (!initial) return;
-    this.mouseEvents();
-  };
-  mouseEvents = () => {
-    let moved, clicked, startPosition, element;
-    let mousedownListener = (event) => {
-      const x = event.pageX;
-      const y = event.pageY;
-      startPosition = {x, y};
-      clicked = true;
-    };
-    let mousemoveListener = (event) => {
-      const x = event.pageX;
-      const y = event.pageY;
-      const innerWidth = window.innerWidth;
-      const innerHeight = window.innerHeight;
-      if (clicked) {
-        event.preventDefault();
-        if (!moved) element = this.createElement();
-        if (!element) return;
-        // Style
-        //// Background color of element
-        element.style.backgroundColor = 'black';
-        //// CSS position of element
-        element.style.position = 'absolute';
-        //// Position of element
-        let right = startPosition.x < x ? startPosition.x : (x > innerWidth ? innerWidth - 5 : x);
-        let left = x < 5 ? 5 : (x < startPosition.x ? x : startPosition.x);
-        if (direction === 'rtl') {
-          right = x < startPosition.x ? (innerWidth - startPosition.x) : (x < innerWidth ? innerWidth - x : 0);
-          left = x < startPosition.x ? (innerWidth - x) : (innerWidth - startPosition.x);
-        }
-        console.log({right, left});
-        console.log({startPosition: startPosition.x});
-        console.log({x});
-        element.style.top = `${startPosition.y < y ? startPosition.y : (y < 5 ? 5 : y)}px`;
-        element.style.bottom = `${y < startPosition.y ? startPosition.y : (y > innerHeight ? innerHeight - 5 : y)}px`;
-        element.style.right = `${right}px`;
-        element.style.left = `${left}px`;
-        //// Width and height of element
-        element.style.width = `${startPosition.x > x ? (x < 0 ? startPosition.x - 5 : startPosition.x - x) : (x > innerWidth ? innerWidth - startPosition.x - 5 : x - startPosition.x)}px`;
-        element.style.height = `${startPosition.y > y ? (y < 0 ? startPosition.y - 5 : startPosition.y - y) : (y > innerHeight ? innerHeight - startPosition.y - 5 : y - startPosition.y)}px`;
-        moved = true;
-      }
-    };
-
-    let mouseupListener = () => {
-      moved = false;
-      clicked = false;
-    };
-
-    document.addEventListener('mousedown', mousedownListener);
-    document.addEventListener('mousemove', mousemoveListener);
-    document.addEventListener('mouseup', mouseupListener);
-
-    // document.addEventListener('mouseup', upListener)
-
-    // release memory
-    // document.removeEventListener('mousedown', downListener)
-    // document.removeEventListener('mousemove', moveListener)
-    // document.removeEventListener('mouseup', upListener)
-  };
+  moved;
+  clicked;
+  startPosition;
+  element;
   createElement = () => {
     // Create elements
     const element = document.createElement('div');
@@ -82,6 +20,66 @@ class ScreenMask {
     return element;
   };
   removeElement = (element) => element.remove();
+  mousedownListener = (event) => {
+    const x = event.pageX;
+    const y = event.pageY;
+    this.startPosition = {x, y};
+    this.clicked = true;
+  };
+  mousemoveListener = (event) => {
+    const x = event.pageX;
+    const y = event.pageY;
+    const innerWidth = window.innerWidth;
+    const innerHeight = window.innerHeight;
+    if (this.clicked) {
+      event.preventDefault();
+      if (!this.moved) this.element = this.createElement();
+      if (!this.element) return;
+      // Style
+      //// Background color of element
+      this.element.style.backgroundColor = 'black';
+      //// CSS position of element
+      this.element.style.position = 'absolute';
+      //// Position of element
+      let right = this.startPosition.x < x ? this.startPosition.x : (x > innerWidth ? innerWidth - 5 : x);
+      let left = x < 5 ? 5 : (x < this.startPosition.x ? x : this.startPosition.x);
+      //////// Get direction of page
+      const direction = getComputedStyle(document.querySelector('body')).direction;
+      if (direction === 'rtl') {
+        right = x < this.startPosition.x ? (innerWidth - this.startPosition.x) : (x < innerWidth ? innerWidth - x : 0);
+        left = x < this.startPosition.x ? (innerWidth - x) : (innerWidth - this.startPosition.x);
+      }
+      this.element.style.top = `${this.startPosition.y < y ? this.startPosition.y : (y < 5 ? 5 : y)}px`;
+      this.element.style.bottom = `${y < this.startPosition.y ? this.startPosition.y : (y > innerHeight ? innerHeight - 5 : y)}px`;
+      this.element.style.right = `${right}px`;
+      this.element.style.left = `${left}px`;
+      //// Width and height of element
+      this.element.style.width = `${this.startPosition.x > x ? (x < 0 ? this.startPosition.x - 5 : this.startPosition.x - x) : (x > innerWidth ? innerWidth - this.startPosition.x - 5 : x - this.startPosition.x)}px`;
+      this.element.style.height = `${this.startPosition.y > y ? (y < 0 ? this.startPosition.y - 5 : this.startPosition.y - y) : (y > innerHeight ? innerHeight - this.startPosition.y - 5 : y - this.startPosition.y)}px`;
+      this.moved = true;
+    }
+  };
+  mouseupListener = () => {
+    this.moved = false;
+    this.clicked = false;
+  };
+  addMouseEvents = () => {
+    document.addEventListener('mousedown', this.mousedownListener);
+    document.addEventListener('mousemove', this.mousemoveListener);
+    document.addEventListener('mouseup', this.mouseupListener);
+  };
+  removeMouseEvents = () => {
+    // release memory
+    document.removeEventListener('mousedown', this.mousedownListener);
+    document.removeEventListener('mousemove', this.mousemoveListener);
+    document.removeEventListener('mouseup', this.mouseupListener);
+  }
+  init = (initial) => {
+    if (!initial) {
+      this.removeMouseEvents();
+      return;
+    }
+    this.addMouseEvents();
+  };
 }
-
 export default ScreenMask;
