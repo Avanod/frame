bef97f307c265ce48113db2c8568e7be76966171
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-01-06 20:35:06 +0330

    feat: add rtl support

--- src/js/screenMask.js
+++ src/js/screenMask.js
@@ -1,8 +1,11 @@
+const direction = getComputedStyle(document.querySelector('body')).direction;
+
 class ScreenMask {
   init = (initial) => {
+    console.log({direction});
     if (!initial) return;
     this.mouseEvents();
-  }
+  };
   mouseEvents = () => {
     let moved, clicked, startPosition, element;
     let mousedownListener = (event) => {
@@ -11,7 +14,6 @@ class ScreenMask {
       startPosition = {x, y};
       clicked = true;
     };
-
     let mousemoveListener = (event) => {
       const x = event.pageX;
       const y = event.pageY;
@@ -27,13 +29,22 @@ class ScreenMask {
         //// CSS position of element
         element.style.position = 'absolute';
         //// Position of element
+        let right = startPosition.x < x ? startPosition.x : (x > innerWidth ? innerWidth - 5 : x);
+        let left = x < 5 ? 5 : (x < startPosition.x ? x : startPosition.x);
+        if (direction === 'rtl') {
+          right = x < startPosition.x ? (innerWidth - startPosition.x) : (x < innerWidth ? innerWidth - x : 0);
+          left = x < startPosition.x ? (innerWidth - x) : (innerWidth - startPosition.x);
+        }
+        console.log({right, left});
+        console.log({startPosition: startPosition.x});
+        console.log({x});
         element.style.top = `${startPosition.y < y ? startPosition.y : (y < 5 ? 5 : y)}px`;
-        element.style.right = `${startPosition.x < x ? startPosition.x : (x > innerWidth ? innerWidth - 5 : x)}px`;
         element.style.bottom = `${y < startPosition.y ? startPosition.y : (y > innerHeight ? innerHeight - 5 : y)}px`;
-        element.style.left = `${x < 5 ? 5 : (x < startPosition.x ? x : startPosition.x)}px`;
+        element.style.right = `${right}px`;
+        element.style.left = `${left}px`;
         //// Width and height of element
         element.style.width = `${startPosition.x > x ? (x < 0 ? startPosition.x - 5 : startPosition.x - x) : (x > innerWidth ? innerWidth - startPosition.x - 5 : x - startPosition.x)}px`;
-        element.style.height = `${startPosition.y > y ? (y < 0 ? startPosition.y - 5 : startPosition.y - y) : (y > innerHeight ? innerHeight - startPosition.y - 5 :  y - startPosition.y)}px`;
+        element.style.height = `${startPosition.y > y ? (y < 0 ? startPosition.y - 5 : startPosition.y - y) : (y > innerHeight ? innerHeight - startPosition.y - 5 : y - startPosition.y)}px`;
         moved = true;
       }
     };
