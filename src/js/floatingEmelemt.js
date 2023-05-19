b2a545dd469462158e916ad8151a9283a4107aef
Nima Kaviyani <nima.kaviyani@gmail.com>
2023-05-19 12:03:15 +0330

    cleanup

--- src/js/floatingEmelemt.js
+++ src/js/floatingEmelemt.js
@@ -204,7 +204,6 @@ const finalizedElements = async () => {
     closeButton.style.visibility = 'hidden';
     closeButton.style.opacity = '0';
     closeButton.innerText = '✖';
-    // closeButton.innerHTML = `<svg viewBox='0 0 12 12' xmlns='http://www.w3.org/2000/svg'><line x1='1' y1='11' x2='11' y2='1' stroke='white' stroke-width='2' /><line x1='1' y1='1' x2='11' y2='11' stroke='white' stroke-width='2' /></svg>`;
     // Append to wrapper
     wrapper.appendChild(closeButton);
     resolve(true);
